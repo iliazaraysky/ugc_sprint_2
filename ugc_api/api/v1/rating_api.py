18626961dda0141554208d736fc004a43f74b683
Ilia Zaraysky <nextontext@yandex.ru>
2022-06-24 11:36:42 +0300

    Разделил логи. Добавил для каждого контейнера свой индекс. Добавил описание в API

--- ugc_api/api/v1/rating_api.py
+++ ugc_api/api/v1/rating_api.py
@@ -11,5 +11,11 @@ router = APIRouter()
 async def user_film_rating(
         data: UserFilmRating,
         rating_event: RatingEventService = Depends(get_rating_event_service)):
+    """
+    - **film_id**: UUID фильма
+    - **user_id**: UUID пользователя
+    - **rating**: float значение.
+    """
+
     post_event = await rating_event.post_event(data)
     return post_event
--- ugc_api/api/v1/user_event_api.py
+++ ugc_api/api/v1/user_event_api.py
@@ -12,12 +12,21 @@ router = APIRouter()
 @router.post("/add-comment")
 async def user_comment(data: UserComment,
                        comment_post: UserEventService = Depends(get_user_event_service)):
+    """
+    Комментарии пользователя к фильму. Обязательные поля
+    - **user_id**: UUID пользователя
+    - **film_id**: UUID фильма
+    - **comment**: Текст комментария
+    """
     user_comment = await comment_post.post_comment(data)
     return user_comment
 
 
 @router.get("/get-likes", response_model=List[UserFilmLike])
 async def get_user_like(get_like: UserEventService = Depends(get_user_event_service)):
+    """
+    Список лайков. Общий.
+    """
     get_user_like = await get_like.get_like_list()
     return get_user_like
 
@@ -25,6 +34,11 @@ async def get_user_like(get_like: UserEventService = Depends(get_user_event_serv
 @router.post("/add-like")
 async def user_like(data: UserFilmLike,
                     like_post: UserEventService = Depends(get_user_event_service)):
-
+    """
+    Лайки пользователя к фильму. Обязательные поля
+    - **user_id**: UUID пользователя
+    - **film_id**: UUID фильма
+    - **like**: Bool значение. False или True
+    """
     user_like = await like_post.post_like(data)
     return user_like
